815039f9c0b79083d65e976b2cc8d795877f82c4
Samarth Galchar <samarthgalchar@gmail.com>
2021-04-27 17:42:11 +0530

    Added scrape.py

---
+++ scrape.py
@@ -0,0 +1,28 @@
+import requests
+import bs4
+import re
+
+res = requests.get('https://myanimelist.net/anime/30/Neon_Genesis_Evangelion/reviews')
+
+soup = bs4.BeautifulSoup(res.text, 'html.parser' )
+
+borderDark = soup.find_all('div', "borderDark")
+
+outputFile = open('output.txt', 'w', encoding="utf-8")
+
+for newSoup in borderDark:
+
+    remove_helpful = newSoup.find_all('a', "button_form")
+    remove_readmore = newSoup.find_all('a', "js-toggle-review-button")
+    profile = newSoup.find_all('a', href=re.compile("https://myanimelist.net/profile/"))
+    reviews = newSoup.find_all('div', "spaceit textReadability word-break pt8 mt8")
+    
+    for helpful,readmore in zip(remove_helpful, remove_readmore):
+        helpful.decompose()
+        readmore.decompose()
+    for a in profile:
+        print(a.get_text("|", strip=True), file = outputFile)
+    for div in reviews:
+        print(div.get_text("|", strip=True), file = outputFile)
+
+outputFile.close()
